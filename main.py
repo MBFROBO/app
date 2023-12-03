15a4d44e3bb009648e097600c650d112bf4598e3
Sergey <khramtsov628@gmail.com>
2023-12-03 20:09:48 +0300

    fix: broadcast with pipes

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 import flet as ft
 import multiprocessing as mp
-from multiprocessing import Value
+from multiprocessing import Value, Queue, Pipe
 
 
-def main(page: ft.Page):
+async def main(page: ft.Page, q):
+    
     page.title = "Flet counter example"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
     txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
 
-    def minus_click(e):
+    async def minus_click(e):
         txt_number.value = str(int(txt_number.value) - 1)
-        page.update()
+        await page.update_async()
 
-    def plus_click(e):
+    async def plus_click(e):
         txt_number.value = str(int(txt_number.value) + 1)
-        page.update()
+        await page.update_async()
 
-    page.add(
+    await page.add_async(
         ft.Row(
             [
                 ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
@@ -29,7 +30,7 @@ def main(page: ft.Page):
     )
 
 if __name__ == '__main__':
-    
+    parent_conn, child_conn = Pipe()
     proc1 = mp.Process(target=ft.app, kwargs={'target':main})
     proc2 = mp.Process(target = ft.app, kwargs=dict(target=main, view=ft.AppView.WEB_BROWSER))
     
